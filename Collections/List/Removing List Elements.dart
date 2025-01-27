18fe923408e748d6969185f69921abbe7e54a514
HosenShahadath <dev.shahadathhosen@gmail.com>
2025-01-27 11:16:53 +0600

    removing list

--- Collections/List/Removing List Elements.dart
+++ Collections/List/Removing List Elements.dart
@@ -3,4 +3,14 @@ void main(){
   print('List before removing element : $list');
   list.remove(30);
   print('List after removing element : $list');
+
+  var list2 = [1, 2, 3, 4, 5, 6, 7];
+  list2.removeAt(4);
+  print(list2);
+  list2.removeLast();
+  print(list2);
+
+  var list3 = [25,30,35,45,55,60,65];
+  list3.removeRange(0, 3);
+  print(list3);
 }

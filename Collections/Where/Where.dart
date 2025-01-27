d66c75743ddb7323fc03aa160ca24f08937cc413
HosenShahadath <dev.shahadathhosen@gmail.com>
2025-01-27 12:44:34 +0600

    where

--- Collections/Where/Where.dart
+++ Collections/Where/Where.dart
@@ -1,3 +1,19 @@
 void main(){
+  List<int> numbers = [2,4,6,8,10,11,12,14];
 
+  List<int> oddNumbers = numbers.where((number)=> number.isOdd).toList();
+  print(oddNumbers);
+
+  List<String> days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+
+  List<String> startWithS = days.where((elemrnt)=> elemrnt.startsWith('S')).toList();
+  print(startWithS);
 }

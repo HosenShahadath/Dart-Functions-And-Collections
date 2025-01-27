13bd6da89ddbdec99f1cdaa9969ded3fbb4872d8
HosenShahadath <dev.shahadathhosen@gmail.com>
2025-01-27 12:10:58 +0600

    map properties

---
+++ Collections/Map/Map.dart
@@ -0,0 +1,10 @@
+void main(){
+  Map<String, String> countryCapital = {
+    'USA' : 'Washinton',
+    'India' : 'New Delhi',
+    'China' : 'Beijing'
+  };
+
+  print(countryCapital);
+  print(countryCapital['USA']);
+}

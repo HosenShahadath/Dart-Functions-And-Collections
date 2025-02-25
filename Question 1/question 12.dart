c1164b6247b5f3aee2469b8bc1a7fd0fe77ab84e
HosenShahadath <dev.shahadathhosen@gmail.com>
2025-02-25 11:34:52 +0600

    question 12

---
+++ Question 1/question 12.dart
@@ -0,0 +1,11 @@
+void main(){
+  int distance = 25;
+  double speed = 40;
+
+  double timeInHours = distance / speed;
+
+  double timeInMinutes = timeInHours * 60;
+
+  print("Time Taken to reach the office: $timeInMinutes minutes");
+
+}

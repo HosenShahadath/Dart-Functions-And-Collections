534e9f92a2fa5f5236739a878c2ad727188b3102
HosenShahadath <dev.shahadathhosen@gmail.com>
2025-03-14 09:12:25 +0600

    qustion 4

--- question 4/question 4.dart
+++ question 4/question 4.dart
@@ -2,5 +2,15 @@ void main (){
   // create a empty list of type String
   List<String> days = [];
 
-  // add the names of the 7 days
+  // add the names of the 7 days3
+  days.add('Monday');
+  days.add('Tuesday');
+  days.add('Wednesday');
+  days.add('Thursday');
+  days.add('Friday');
+  days.add('Saturday');
+  days.add('Sunday');
+
+  // print all the days
+  print(days);
 }
